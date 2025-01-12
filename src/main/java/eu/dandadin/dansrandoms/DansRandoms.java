43f48f7704d28015edae4095c431e21f89637ab7
Dan Simanek <dandadin@gmail.com>
2025-01-12 22:12:15 +0100

    Empty mod

---
+++ src/main/java/eu/dandadin/dansrandoms/DansRandoms.java
@@ -0,0 +1,16 @@
+package eu.dandadin.dansrandoms;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DansRandoms implements ModInitializer {
+	public static final String MOD_ID = "dansrandoms";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/eu/dandadin/dansrandoms/DansRandomsClient.java
@@ -0,0 +1,10 @@
+package eu.dandadin.dansrandoms;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DansRandomsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
